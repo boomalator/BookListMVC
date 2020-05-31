1aa20320b802637f2d46bda0377ba40fd51e6ac7
BP (Home) <bph@byronetta.com>
2020-05-31 01:02:18 -0600

    Login Page and Cookies https://www.youtube.com/watch?v=9d8DXXc71RI

--- BookListMVC/Controllers/AccountController.cs
+++ BookListMVC/Controllers/AccountController.cs
@@ -20,6 +20,35 @@ namespace BookListMVC.Controllers
             this.signInManager = signInManager;
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(ViewModels.LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync( 
+                    userName:model.Email, 
+                    password:model.Password, 
+                    isPersistent:model.RememberMe,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+
+            return View(model);
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
